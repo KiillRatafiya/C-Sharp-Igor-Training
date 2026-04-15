2e2817429e8a9d068d4508d73f1a5474f40cb2cc
KirillRatafiya <ratafiya@gmail.com>
2026-04-15 19:19:36 +0300

    Recreated task 5

--- Task5 InheritanceEmployee/Employee.cs
+++ Task5 InheritanceEmployee/Employee.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task5_Employee
+namespace Task5_InheritanceEmployee
 {
     class Employee
     {
-        public string Name { get; set; } 
+        public string Name { get; set; }
         public int Salary { get; set; }
         public virtual void Work()
         {
--- Task5 InheritanceEmployee/Program.cs
+++ Task5 InheritanceEmployee/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Task5InheritanceEmployee
+﻿namespace Task5_InheritanceEmployee
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Employee employee = new Employee();
+            employee.Work();
+            Employee manager = new Manager();
+            manager.Work();
+            Employee developer = new Developer();
+            developer.Work();
         }
     }
 }
