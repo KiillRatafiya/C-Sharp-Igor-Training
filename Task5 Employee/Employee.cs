896621f461285a173b109fdbc0c239fe1de31b11
KirillRatafiya <ratafiya@gmail.com>
2026-04-15 19:12:16 +0300

    adeed Employee project

---
+++ Task5 Employee/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_Employee
+{
+    class Employee
+    {
+        public string Name { get; set; } 
+        public int Salary { get; set; }
+        public virtual void Work()
+        {
+            Console.WriteLine("Employee works");
+        }
+    }
+    class Manager : Employee
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Manager - manages team");
+        }
+    }
+
+    class Developer : Employee
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Developer - writing code");
+        }
+    }
+}
