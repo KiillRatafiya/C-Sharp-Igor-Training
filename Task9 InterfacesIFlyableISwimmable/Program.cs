3658514b76ee6fea97128b80b33cf75cd37e0b50
KirillRatafiya <ratafiya@gmail.com>
2026-04-16 12:57:15 +0300

    Added one more solutions for Shape using Abstract class

---
+++ Task9 InterfacesIFlyableISwimmable/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task9_InterfacesIFlyableISwimmable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
