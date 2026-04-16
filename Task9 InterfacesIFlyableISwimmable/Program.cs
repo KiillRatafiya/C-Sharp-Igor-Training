2d2a43106c6d0cd929d3962c9aab1bcc78de1399
KirillRatafiya <ratafiya@gmail.com>
2026-04-16 14:53:13 +0300

    Task9

--- Task9 InterfacesIFlyableISwimmable/Program.cs
+++ Task9 InterfacesIFlyableISwimmable/Program.cs
@@ -1,10 +1,56 @@
 ﻿namespace Task9_InterfacesIFlyableISwimmable
 {
-    internal class Program
+    interface IFlyable
     {
-        static void Main(string[] args)
+        public void Fly();
+    }
+
+    interface ISwimmable
+    {
+        public void Swim();
+    }
+
+    class Duck : IFlyable, ISwimmable
+    {
+        public void Fly()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I'm Duck, I can fly");
+        }
+        public void Swim()
+        {
+            Console.WriteLine("I'm Duck, I can swim");
+        }
+    }
+    class Fish : ISwimmable
+    {
+        public void  Swim()
+        {
+            Console.WriteLine("I'm Fish, I can only swim");
+        }
+    }
+    class Bird : IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("I'm Bird, I can fly");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Duck duck = new Duck();
+            IFlyable flyer = duck;
+            ISwimmable swimmer = duck;
+            duck.Swim();
+            duck.Fly();
+
+            IFlyable bird = new Bird();
+            bird.Fly();
+
+            ISwimmable fish = new Fish();
+            fish.Swim();
         }
     }
 }
