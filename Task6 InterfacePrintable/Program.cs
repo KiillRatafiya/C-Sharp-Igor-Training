e8364bc2d2f4c67382f5fe7d35c686d83465fb0d
KirillRatafiya <ratafiya@gmail.com>
2026-04-16 10:38:17 +0300

    Added tasks 6-7

---
+++ Task6 InterfacePrintable/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Task6_InterfacePrintable
+{
+    interface IPrintable
+    {
+        void Print();
+    }
+    class Book : IPrintable
+    {
+        public void Print()
+        {
+            Console.WriteLine("Print book");
+        }
+    }
+
+    class Document : IPrintable
+    {
+        public void Print()
+        {
+            Console.WriteLine("Print document");
+        }
+    }
+
+    class Photo : IPrintable
+    {
+        public void Print()
+        {
+            Console.WriteLine("Print photo");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            //Calling method for each object
+            Book book = new Book();
+            book.Print();
+
+            Document document = new Document();
+            document.Print();
+
+            Photo photo = new Photo();
+            photo.Print();
+
+            //Calling method for each object via loop
+            IPrintable[] items = { new Book(), new Document(), new Photo()};
+            foreach(var i in items)
+            {
+                i.Print();                
+            }   
+        }
+    }
+}
---
+++ Task7 InterfaceIMovable/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Task7_InterfaceIMovable
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is driving");
+        }
+    }
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is riding");
+        }
+
+    }
+    class Person : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Person is going");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            IMovable[] movable = {new Car(), new Bicycle(), new Person()};
+            foreach (var i in movable)
+            {
+                i.Move();
+            }
+        }
+    }
+}
